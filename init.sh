af7ec3be8499cc13d31b0e51c36151fa9d3fac48
Robin Roth <robin.roth@kit.edu>
2017-01-02 15:03:14 +0100

    Use service cmd for service handling

--- init.sh
+++ init.sh
@@ -16,14 +16,14 @@ echo "Europe/Berlin" | tee /etc/timezone
 dpkg-reconfigure --frontend noninteractive tzdata
 
 #restart apache
-systemctl restart apache2
+service apache2 restart
 
 #start rrdcached as user nobody
 #the limited.sock is for the webfrontend
 su nobody -c 'rrdcached -F -p /tmp/rrdcached.pid -s www-data -m 664 -l unix:/tmp/rrdcached.sock -s nogroup -m 777 -P FLUSH,STATS,HELP -l unix:/tmp/rrdcached.limited.sock -b /var/lib/ganglia/rrds -B -w 1800 -z 1800 -f 3600 ' -s /bin/sh
 
 #start gmond
-systemctl start gmond
+service ganglia-monitor start
 
 #start gmetad in debugmode
 gmetad -d 1 -p /var/run/gmetad.pid -c /etc/ganglia/gmetad.conf
