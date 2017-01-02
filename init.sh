7d574b68058b35b17ed4603e6f90d3abc6c8ec6f
Robin Roth <robin.roth@kit.edu>
2017-01-02 15:33:37 +0100

    Get rid of the limited socket

--- init.sh
+++ init.sh
@@ -9,7 +9,7 @@ usermod -aG www-data nobody
 
 #rrd-socketfile
 export RRDCACHED_ADDRESS=unix:/tmp/rrdcached.sock
-echo '<?php $conf[rrdcached_socket] = "unix:/tmp/rrdcached.limited.sock"; ?>' > /etc/ganglia-webfrontend/conf.php
+echo '<?php $conf["rrdcached_socket"] = "unix:/tmp/rrdcached.sock"; ?>' > /etc/ganglia-webfrontend/conf.php
 
 #german timezone
 echo "Europe/Berlin" | tee /etc/timezone
@@ -20,7 +20,7 @@ service apache2 restart
 
 #start rrdcached as user nobody
 #the limited.sock is for the webfrontend
-su nobody -c 'rrdcached -F -p /tmp/rrdcached.pid -s www-data -m 664 -l unix:/tmp/rrdcached.sock -s nogroup -m 777 -P FLUSH,STATS,HELP -l unix:/tmp/rrdcached.limited.sock -b /var/lib/ganglia/rrds -B -w 1800 -z 1800 -f 3600 ' -s /bin/sh
+su nobody -c 'rrdcached -F -p /tmp/rrdcached.pid -s www-data -m 664 -l unix:/tmp/rrdcached.sock -b /var/lib/ganglia/rrds -B -w 1800 -z 1800 -f 3600 ' -s /bin/sh
 
 #start gmond
 service ganglia-monitor start
